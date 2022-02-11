b45e8e36b434cd8dbb6acba6c904680470b0126e
Bartłomiej Juda <bartekjkk@gmail.com>
2022-02-11 12:12:56 +0100

    bart-10| przypadek testowy dla braku srodkow na koncie

--- Test/Business_classes/money_tranfer_usecase_test.py
+++ Test/Business_classes/money_tranfer_usecase_test.py
@@ -7,23 +7,24 @@ from Business_classes.Money_Transfer_Command import MoneyTransferCommand
 class MoneyTransferUseCaseTest(unittest.TestCase):
     repo = {
         "123": Account('123', 'Bartek', 444),
-        "456": Account('456', 'Łukasz', 0)
+        "456": Account('456', 'Łukasz', 0),
+        "789": Account('789', 'Michał', 60)
     }
 
     def test_one_account_locked(self):
         # Given
         self.repo["123"] = Account('123', 'Bartek', 444, '354')
-        usecase = MoneyTransferUseCase(self.repo)
+        use_case = MoneyTransferUseCase(self.repo)
         # When
-        comamnd = MoneyTransferCommand('123', '456', 222)
+        command = MoneyTransferCommand('123', '456', 222)
         # Then
         with self.assertRaises(Exception):
-            usecase.transfer(comamnd)
+            use_case.transfer(command)
 
-    def test_two_accounts_locked(self):
-        self.repo["123"] = Account('123', 'Bartek', 444, '456')
-        self.repo["456"] = Account('456', 'Łukasz', 0, '123')
+    def test_one_account_no_money(self):
+        self.repo["123"] = Account("123", "Bartek", 50)
+        self.repo["456"] = Account("456", "Lukasz", 0)
         use_case = MoneyTransferUseCase(self.repo)
-        command = MoneyTransferCommand('123', '456', 222)
+        command = MoneyTransferCommand("456", "123", 100)
         with self.assertRaises(Exception):
             use_case.transfer(command)
