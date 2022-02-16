ec9a51380f716bc5b66d6aa11bf119c3d2ec807f
Bartłomiej Juda <bartekjkk@gmail.com>
2022-02-16 01:07:58 +0100

    bart-17 |

--- Domain_classes/Account.py
+++ Domain_classes/Account.py
@@ -1,46 +1,22 @@
-import random
-
-class Account():
-    def __init__(self, account_id,name,balance:int, open_transaction=None):
+class Account:
+    def __init__(self, account_id, name, balance: int, open_transaction=None):
         self.account_id = account_id
         self.name = name
         self.open_transaction = open_transaction
         self.balance = balance
 
     def __str__(self):
-        return f" numer konta:{self.account_id}, imie:{self.name}, saldo:{self.balance} złotych"
+        return f" account number:{self.account_id}, name:{self.name}, balance:{self.balance} pln"
 
     def add_money(self):
         pass
 
-
-
-
     def sub_money(self):
         pass
 
-
-    def block_account(self,transfer_id):
+    def block_account(self, transfer_id):
         # if open_transaction not null throw exeption
         pass
 
-    def unlock_account(self,tranfer_id):
+    def unlock_account(self, tranfer_id):
         pass
-
-
-names= ["Kasia","Bartek","Magda","Maja","Tomek","Michał","Łukasz","Martyna"]
-
-def generate_account():
-    random_list = [random.randint(0, 9) for number in range(26)]
-    str_account_number = ""
-    for number in random_list:
-        str_account_number += str(number)
-    int_account_number = int(str_account_number)
-    name= random.choice(names)
-    balance= random.randint(-100,9999)
-    user_account=Account(int_account_number,name,balance)
-    return user_account
-
-konto= generate_account()
-
-print(konto)
--- Test/Business_classes/money_tranfer_usecase_test.py
+++ Test/Business_classes/money_tranfer_usecase_test.py
@@ -1,33 +1,29 @@
 import unittest
 from Domain_classes.Account import Account
-from Domain_classes.Transaction import Transaction
-from Business_classes.Money_Transfer_Usecase import MoneyTransferUsecase
+from Business_classes.Money_Transfer_Usecase import MoneyTransferUseCase
 from Business_classes.Money_Transfer_Command import MoneyTransferCommand
 
-class MoneyTransferUseCaseTest(unittest.TestCase):
 
-    repo={
-        "123": Account('123','Bartek',444),
-        "456": Account('456','Łukasz',0)
+class MoneyTransferUseCaseTest(unittest.TestCase):
+    repo = {
+        "123": Account('123', 'Bartek', 444),
+        "456": Account('456', 'Łukasz', 0)
     }
 
     def test_one_account_locked(self):
         # Given
-        self.repo["123"]=Account('123','Bartek',444,'354')
-        usecase= MoneyTransferUsecase(self.repo)
+        self.repo["123"] = Account('123', 'Bartek', 444, '354')
+        usecase = MoneyTransferUseCase(self.repo)
         # When
-        comamnd= MoneyTransferCommand('123','456',222)
+        comamnd = MoneyTransferCommand('123', '456', 222)
         # Then
         with self.assertRaises(Exception):
             usecase.transfer(comamnd)
 
-
-            # pozostałe przypadki, pusty numer konta itp
-
-        #
-        #
-        #
-
-        #  dwa konta sa zablokowane
-        #
-
+    def test_two_accounts_locked(self):
+        self.repo["123"] = Account('123', 'Bartek', 444, '456')
+        self.repo["456"] = Account('456', 'Łukasz', 0, '123')
+        use_case = MoneyTransferUseCase(self.repo)
+        command = MoneyTransferCommand('123', '456', 222)
+        with self.assertRaises(Exception):
+            use_case.transfer(command)
