dba6b49902608c30780f3f27e2d2ea30e2b42b70
Bartłomiej Juda <bartekjkk@gmail.com>
2022-02-27 22:03:20 +0100

    Lukasz-1, code refactored

--- Test/Business_classes/money_tranfer_usecase_test.py
+++ Test/Business_classes/money_tranfer_usecase_test.py
@@ -8,7 +8,7 @@ class MoneyTransferUseCaseTest(unittest.TestCase):
     repo = {
         "123": Account('123', 'Bartek', 444),
         "456": Account('456', 'Łukasz', 0),
-        "789": Account('789', 'Michał', 60)
+        "789": Account('789', 'Michał', 60),
     }
 
     def test_one_account_locked(self):
@@ -25,9 +25,11 @@ class MoneyTransferUseCaseTest(unittest.TestCase):
         self.repo["123"] = Account("123", "Bartek", 50)
         self.repo["456"] = Account("456", "Lukasz", 0)
         use_case = MoneyTransferUseCase(self.repo)
-        command = MoneyTransferCommand("456", "123", 100)
+        command = MoneyTransferCommand("123", "456", 100)
         with self.assertRaises(Exception):
             use_case.transfer(command)
+        command = MoneyTransferCommand("123", "456", 50)
+        use_case.transfer(command)
 
     def test_two_accounts_valid(self):
         self.repo["123"] = Account("123", "Bartek", 444)
@@ -36,6 +38,7 @@ class MoneyTransferUseCaseTest(unittest.TestCase):
         command = MoneyTransferCommand("123", "456", 100)
         use_case.transfer(command)
         self.assertEqual(self.repo["123"].balance, 344)
+        self.assertEqual(self.repo["456"].balance, 100)
 
     def test_two_accounts_locked(self):
         self.repo["123"] = Account('123', 'Bartek', 444, '456')
@@ -45,42 +48,47 @@ class MoneyTransferUseCaseTest(unittest.TestCase):
         with self.assertRaises(Exception):
             use_case.transfer(command)
 
-    def test_one_account_balance_is_None(self):
-        self.repo["123"] = Account("123", "Bartek",444)
-        use_case = MoneyTransferUseCase(self.repo)
-        command = MoneyTransferCommand("123", "456", None)
-        use_case.transfer(command)
-        with self.assertRaises(Exception):
-            use_case.transfer(command)
+    # TODO  move to command_test
+    # def test_one_account_balance_is_None(self):
+    #     self.repo["123"] = Account("123", "Bartek",444)
+    #     use_case = MoneyTransferUseCase(self.repo)
+    #     command = MoneyTransferCommand("123", "456", None)
+    #     use_case.transfer(command)
+    #     with self.assertRaises(Exception):
+    #         use_case.transfer(command)
 
-    def test_one_account_number_empty(self):
-        self.repo["123"] = Account("123", "Bartek", 50)
-        self.repo["456"] = Account("456", "Lukasz", 0)
-        use_case = MoneyTransferUseCase(self.repo)
-        command = MoneyTransferCommand("123", "", 100)
-        with self.assertRaises(Exception):
-            use_case.transfer(command)
+    # TODO MOVE TO money_tranfer_command_test
+    # def test_one_account_number_empty(self):
+    #     self.repo["123"] = Account("123", "Bartek", 50)
+    #     self.repo["456"] = Account("456", "Lukasz", 0)
+    #     use_case = MoneyTransferUseCase(self.repo)
+    #     command = MoneyTransferCommand("123", "", 100)
+    #     with self.assertRaises(Exception):
+    #         use_case.transfer(command)
 
-    def test_one_account_number_is_None(self):
-        self.repo["123"] = Account("123", "Bartek", 50)
-        self.repo["456"] = Account("456", "Lukasz", 0)
-        use_case = MoneyTransferUseCase(self.repo)
-        command = MoneyTransferCommand(a_account_number="123", b_account_number=None, money_amount=100)
-        with self.assertRaises(Exception):
-            use_case.transfer(command)
+    # TODO MOve to Money_tranfer_commnad_test
+    # def test_one_account_number_is_None(self):
+    #     self.repo["123"] = Account("123", "Bartek", 50)
+    #     self.repo["456"] = Account("456", "Lukasz", 0)
+    #     use_case = MoneyTransferUseCase(self.repo)
+    #     command = MoneyTransferCommand(a_account_number="123", b_account_number=None, money_amount=100)
+    #     with self.assertRaises(Exception):
+    #         use_case.transfer(command)
 
-    def test_negative_money_amount(self):
-        self.repo["123"] = Account("123", "Bartek", 444)
-        self.repo["456"] = Account("456", "Lukasz", 0)
-        use_case = MoneyTransferUseCase(self.repo)
-        command = MoneyTransferCommand("123", "456", -10)
-        with self.assertRaises(Exception):
-            use_case.transfer(command)
+    # TODO move to Money_command_test
+    # def test_negative_money_amount(self):
+    #     self.repo["123"] = Account("123", "Bartek", 444)
+    #     self.repo["456"] = Account("456", "Lukasz", 0)
+    #     use_case = MoneyTransferUseCase(self.repo)
+    #     command = MoneyTransferCommand("123", "456", -10)
+    #     with self.assertRaises(Exception):
+    #         use_case.transfer(command)
 
-    def test_zero_money_amount(self):
-        self.repo["123"] = Account("123", "Bartek", 444)
-        self.repo["456"] = Account("456", "Lukasz", 0)
-        use_case = MoneyTransferUseCase(self.repo)
-        command = MoneyTransferCommand("123", "456", 0)
-        with self.assertRaises(Exception):
-            use_case.transfer(command)
+    # TODO move to money_transfer_command_test
+    # def test_zero_money_amount(self):
+    #     self.repo["123"] = Account("123", "Bartek", 444)
+    #     self.repo["456"] = Account("456", "Lukasz", 0)
+    #     use_case = MoneyTransferUseCase(self.repo)
+    #     command = MoneyTransferCommand("123", "456", 0)
+    #     with self.assertRaises(Exception):
+    #         use_case.transfer(command)
--- Transaction.py
+++ Transaction.py
@@ -1,5 +1,6 @@
-class Transaction():
-    def __init__(self,a_account_id,b_account_id,money_amount,date,uuid):
+class Transaction:
+    def __init__(self, a_account_id, b_account_id, money_amount, date, uuid, status):
+        self.status = status
         self.a_account_id = a_account_id
         self.b_account_id = b_account_id
         self.money_amount = money_amount
