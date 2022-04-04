6b2bf55326cbceaa2367064d694e26ec1f7d8b9d
Bartłomiej Juda <bartekjkk@gmail.com>
2022-04-04 21:10:45 +0200

    fucked up up in

--- Repository/File_Accounts_Repository.py
+++ Repository/File_Accounts_Repository.py
@@ -15,39 +15,61 @@ class FileAccountRepositoryMeta(type):
 
 class FileAccountRepository(metaclass=FileAccountRepositoryMeta):
 
-
     def load_repo_from_file(self):
-        with open ("accounts.bartek", "r", encoding="utf-8") as file:
-            accounts_dict = json.load(file)
-        return accounts_dict
-
+        accounts_list = []
+        with open("accounts.bartek", "r", encoding="utf-8") as file:
+            dict_list = json.load(file)
+            for account in dict_list:
+                serialized_account = Account(account_number=account["account_number"],
+                                             name=account["name"],
+                                             balance=account["account_balance"],
+                                             id=account["id"])
+                accounts_list.append(serialized_account)
+        return accounts_list
 
     def save_repo_to_file(self, accounts_dict):
-        with open ("accounts.bartek", "w", encoding="utf-8") as file:
+        with open("accounts.bartek", "w", encoding="utf-8") as file:
             json.dump(accounts_dict, file, indent=4)
 
     def get_all(self):
-        with open('accounts.bartek', "r", encoding='utf-8') as acc_file:
-            accounts_dict = json.load(acc_file)
-        return accounts_dict.values()
+        return self.load_repo_from_file()
 
     def find_by_account_number(self, account_number: str):
-        for account in self._accounts.values():
+        accounts = self.load_repo_from_file()
+        for account in accounts:
             if account.account_number == account_number:
                 return account
         raise AccountRepositoryException(" Account not found")
 
     def up_in(self, account: Account):
-        id = uuid.uuid4()
-        account.id = id
-        accounts_dict = self.load_repo_from_file()
-        print(accounts_dict)
+        accounts_list = self.load_repo_from_file()
+        if account.id is None:
+            id = uuid.uuid4()
+            account.id = id
+        else:
+            for index, account_from_list in enumerate(accounts_list):
+                if account.account_number == account_from_list.account_number:
+                    pop_account = accounts_list.pop(index)
+                    print(pop_account)
+                    pop_account = account
+                    # pop_account = Account(account.account_number, account.name, account.balance)
 
-        accounts_dict['accounts'].append(account.serialize())
-        self.save_repo_to_file(accounts_dict)
-        print(accounts_dict)
 
+                elif account.id is not None and account.id not in account_from_list.id:
+                    raise AccountRepositoryException(" Account not found")
 
+        # print(f" lista po del {accounts_list}")
+        # print(f" konto ktore przekazujemy : {account} o id: {account.id}")
+        # account = account.serialize()
+        # print(f"konto po serializacji {account}")
+        # accounts_list.append(account)
+        # print(f"lista po dodaniu tego konta {accounts_list}")
+        #
+        # self.save_repo_to_file(accounts_list)
+
+        # accounts_list = self.load_repo_from_file()
+        # if account
+        # self.save_repo_to_file(accounts_dict)
 
         # id = account.id
         # if id is not None and id not in self._accounts:
@@ -57,12 +79,15 @@ class FileAccountRepository(metaclass=FileAccountRepositoryMeta):
         #     account.id = id
         # self._accounts[id] = account
 
-    #
-    # def delete(self, account_number: str):
-    #     account = self.find_by_account_number(account_number)
-    #     with open ("accounts.bartek", "w", encoding="utf-8") as acc_file:
-    #         json.
-        # del self._accounts[account.id]
+    def delete(self, account_number: str):
+        account_to_delete = self.find_by_account_number(account_number)
+        account_dict = self.load_repo_from_file()
+        for accounts in account_dict.values():
+            for account in accounts:
+                print(account)
+                # if account_to_delete.account_number == account.account_number:
+                #     del account_dict[accounts][account]
+        # del account_dict.values[account]
 
 
 class AccountRepositoryException(Exception):
